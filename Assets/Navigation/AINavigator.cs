e082a79e3f303d86ec9f173b9bd2f5a3a880313f
WaniFi <chaos0824@naver.com>
2020-03-20 23:31:21 +0900

    AI Prefab의 Navgation 관리 스크립트 구현 중

---
+++ Assets/Navigation/AINavigator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AINavigator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Target;
+    // AI의 Navigation 객체
+    private NavMeshAgent Agent;
+
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        SetDirection(Target.position);
+    }
+
+    // AI에게 매개변수 좌표로 이동하도록 함
+    private void SetDirection(Vector3 _dir)
+    {
+        Agent.SetDestination(_dir);
+    }
+}
