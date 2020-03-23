47d4d1574c19b4ca2cdd237433c68cfcdac9cc3b
WaniFi <chaos0824@naver.com>
2020-03-23 21:37:50 +0900

    Terrain 정보 저장 스크립트 추가

---
+++ Assets/Generating Terrain/TerrainInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainInfo : MonoBehaviour
+{
+    [SerializeField]
+    private int width, height, depth;
+    public int Width
+    { get => width; }
+    public int Height
+    { get => height; }
+    public int Depth
+    { get => depth; }
+    [SerializeField]
+    private float heightLimit;
+    public float HeightLimit
+    { get => heightLimit; }
+
+    public static TerrainInfo Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
