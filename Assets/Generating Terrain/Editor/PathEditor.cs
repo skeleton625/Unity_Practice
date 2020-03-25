433da539337a87560272e36fa03235c598306fbe
WaniFi <chaos0824@naver.com>
2020-03-25 23:02:13 +0900

    GUI로 강을 생성할 수 있는 Editor 스크립트 구현

---
+++ Assets/Generating Terrain/Editor/PathEditor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+
+    PathCreator creator;
+    Path path;
+
+    void OnSceneGUI()
+    {
+        Input();
+        Draw();
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            RaycastHit info;
+            if(Physics.Raycast(mouseRay.origin, mouseRay.direction, out info, 1000f))
+            {
+                Undo.RecordObject(creator, "Add segment");
+                path.AddSegment(info.point);
+            }
+        }
+    }
+
+    void Draw()
+    {
+
+        for (int i = 0; i < path.NumSegments; i++)
+        {
+            Vector3[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.black;
+            // 시작 부분의 기준점과 회전점
+            Handles.DrawLine(points[1], points[0]);
+            // 끝 부분의 기준점과 회전점
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+        }
+
+        Handles.color = Color.red;
+        for (int i = 0; i < path.NumPoints; i++)
+        {
+            Vector3 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector3.zero, Handles.CylinderHandleCap);
+            newPos.y = 0;
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");
+                path.MovePoint(i, newPos);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+}
