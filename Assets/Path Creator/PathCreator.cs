c067f5349c2692259a0f8c9f651072e1edc0a908
WaniFi <chaos0824@naver.com>
2020-03-27 20:54:44 +0900

    기준 점, 회전 점, 선에 대한 색, 크기 변수 추가

--- Assets/Path Creator/PathCreator.cs
+++ Assets/Path Creator/PathCreator.cs
@@ -7,8 +7,23 @@ public class PathCreator : MonoBehaviour
     [HideInInspector]
     public Path path;
 
+    // Inspector 창을 통해 조절할 수 있는 변수들
+    public Color anchorCol = Color.red;
+    public Color controlCol = Color.white;
+    public Color segmentCol = Color.green;
+    public Color selectedSegmentCol = Color.yellow;
+    public float anchorDiameter = .1f;
+    public float controlDiameter = .075f;
+    public bool displayControlPoints = true;
+
     public void CreatePath()
     {
         path = new Path(transform.position);
+    }
+
+    // Inspector 창에서 스크립트 컴포넌트를 Reset할 경우 실행
+    private void Reset()
+    {
+        CreatePath();
     }
 }
