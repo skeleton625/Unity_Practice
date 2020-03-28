f1bd1a1737f145ddbc468656175cb711826ef280
WaniFi <chaos0824@naver.com>
2020-03-28 22:27:53 +0900

    곡선에서 두 점 사이의 점을 구하는 스크립트 구현

---
+++ Assets/Path Creator/Bezier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Bezier
+{
+    public static Vector3 EvaluateQuadratic(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        // a, b 지점 사이의 t * 100 % 에 위치한 좌표
+        Vector3 p0 = Vector3.Lerp(a, b, t);
+        // b, c 지점 사이의 t * 100 % 에 위치한 좌표
+        Vector3 p1 = Vector3.Lerp(b, c, t);
+        // p0, p1 지점 사이의 t * 100 % 에 위치한 좌표
+        return Vector3.Lerp(p0, p1, t);
+    }
+
+    public static Vector3 EvaluateCubic(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
+    {
+        Vector3 p0 = EvaluateQuadratic(a, b, c, t);
+        Vector3 p1 = EvaluateQuadratic(b, c, d, t);
+        return Vector3.Lerp(p0, p1, t);
+    }
+}
