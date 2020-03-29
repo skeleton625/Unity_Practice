5bc19923d24ed14b0df42e9b7c0bf19f5403513f
WaniFi <chaos0824@naver.com>
2020-03-29 20:28:53 +0900

    TerrainData의 Heights 배열을 전역 변수로 정의

--- Assets/Generating Terrain/TerrainInfo.cs
+++ Assets/Generating Terrain/TerrainInfo.cs
@@ -6,25 +6,45 @@ public class TerrainInfo : MonoBehaviour
 {
     [SerializeField]
     private int width, height, depth, scale;
+    [SerializeField]
+    private Terrain FieldTerrain;
+    [SerializeField]
+    private float heightLimit;
+    private float[,] hArray;
+    public float this[int x, int z]
+    {
+        get => hArray[x, z];
+        set => hArray[x, z] = value;
+    }
+
     public int Width
     { get => width; }
     public int Height
     { get => height; }
     public int Depth
     { get => depth; }
-
     public int Scale
     { get => scale; }
-    [SerializeField]
-    private float heightLimit;
     public float HeightLimit
     { get => heightLimit; }
 
-    public static TerrainInfo Instance;
-
     private void Awake()
     {
-        Instance = this;
-        TerrainArray = new float[width, height];
+        hArray = FieldTerrain.terrainData.GetHeights(0, 0, width, height);
+    }
+
+    public void ApplyPreTerrainHeights()
+    {
+        TerrainData data = FieldTerrain.terrainData;
+        data.heightmapResolution = width + 1;
+        data.size = new Vector3(width, depth, height);
+
+        data.SetHeights(0, 0, hArray);
+        FieldTerrain.terrainData = data;
+    }
+
+    public float[,] GetPreHeights()
+    {
+        return hArray;
     }
 }
