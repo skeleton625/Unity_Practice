124b87e06405ada6f61e1219b1a2fed31cf0a649
WaniFi <chaos0824@naver.com>
2020-04-01 00:35:32 +0900

    랜덤 Terrain 높낮이 생성 코드 추가

--- Assets/Generating Terrain/TerrainGenerator.cs
+++ Assets/Generating Terrain/TerrainGenerator.cs
@@ -8,18 +8,19 @@ public class TerrainGenerator : MonoBehaviour
     private TerrainInfo Info;
     [SerializeField]
     private RiverGenerator River;
+    [SerializeField]
+    private float OffsetX, OffsetZ;
 
     private void Awake()
     {
+        // 기존 PerlinNoize에 변칙성을 추가
+        OffsetX = Random.Range(0, 9999f);
+        OffsetZ = Random.Range(0, 9999f);
+        // PerlinNoize가 들어간 Terrain 생성
         GenerateTerrain();
     }
 
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(1))
-            GenerateTerrain();
-    }
-
+    // 전체 Terrain의 형태를 구현하는 함수 
     private void GenerateTerrain()
     {
         GenerateDefaultHeights();
@@ -28,6 +29,7 @@ public class TerrainGenerator : MonoBehaviour
         Info.ApplyPreTerrainHeights();
     }
 
+    // 랜덤으로 Terrain의 각 위치를 조정해 Terrain의 형태를 구현하는 함수
     private void GenerateDefaultHeights()
     {
         int _width = Info.Width, _height = Info.Height;
@@ -36,7 +38,9 @@ public class TerrainGenerator : MonoBehaviour
         for(int x = 0; x < _width; x++)
             for(int z = 0; z < _height; z++)
             {
+                /* Terrain 내 모든 부분에 대해 랜덤 높낮이를 정의 */
                 _depth = CalculateRandomHeight(x, z);
+                /* 랜덤 값이 기준 높이보다 낮을 경우, 기준 높이로 통일 */
                 if (_depth < _limit)
                     Info[z, x] = _limit;
                 else
@@ -45,10 +49,11 @@ public class TerrainGenerator : MonoBehaviour
                 
     }
 
+    // PerlinNoise 알고리즘을 통한 Terrain 높낮이 정의 함수
     private float CalculateRandomHeight(float _x, float _z)
     {
-        float _xCoord = _x / Info.Width * Info.Scale;
-        float _zCoord = _z / Info.Height * Info.Scale;
+        float _xCoord = _x / Info.Width * Info.Scale + OffsetX;
+        float _zCoord = _z / Info.Height * Info.Scale + OffsetZ;
 
         return Mathf.PerlinNoise(_xCoord, _zCoord);
     }
