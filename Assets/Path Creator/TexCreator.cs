4aed8a82f4d39c4e460ffb4b039b7ca430810644
WaniFi <chaos0824@naver.com>
2020-04-01 00:40:33 +0900

    구현된 Path 객체에 따라 경로에 맞는 Texture를 생성하도록 CreateTexMesh 함수 수정

--- Assets/Path Creator/TexCreator.cs
+++ Assets/Path Creator/TexCreator.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
 public class TexCreator : MonoBehaviour
 {
     private enum PathSpace { xyz, xy, xz };
 
+    [SerializeField]
+    private GameObject NewMesh;
     [SerializeField]
     private float texWidth;
     [SerializeField, Range(.05f, 1.5f)]
@@ -18,41 +18,52 @@ public class TexCreator : MonoBehaviour
     {
         Path path = GetComponent<PathCreator>().path;
         Vector3[] points = path.CalculateEvenlySpacedPoitns(spacing);
-        GetComponent<MeshFilter>().mesh = CreateTexMesh(points);
+        GameObject RiverMesh = Instantiate(NewMesh, Vector3.zero, Quaternion.identity);
+        RiverMesh.transform.position = new Vector3(0, -1.1f, 0);
+        RiverMesh.GetComponent<MeshFilter>().mesh = CreateTexMesh(points);
     }
 
-    Mesh CreateTexMesh(Vector3[] points)
+    private Mesh CreateTexMesh(Vector3[] points)
     {
         Vector3[] verts = new Vector3[points.Length * 2];
-        int[] tris = new int[2 * (points.Length - 1) * 3];
-        int vertIndex = 0, triIndex = 0;
+        Vector2[] uvs = new Vector2[verts.Length];
+        int numTris = 2 * (points.Length - 1);
+        int[] tris = new int[numTris * 3];
+        int vertIndex = 0;
+        int triIndex = 0;
 
-        for(int i = 0; i < points.Length; i++)
+        for (int i = 0; i < points.Length; i++)
         {
             Vector3 forward = Vector3.zero;
-            if(i < points.Length - 1)
+            if (i < points.Length - 1)
             {
-                forward += points[i + 1] - points[i];
+                forward += points[(i + 1) % points.Length] - points[i];
             }
-            if(i > 0)
+            if (i > 0)
             {
-                forward -= points[i] - points[i - 1];
+                forward += points[i] - points[(i - 1 + points.Length) % points.Length];
             }
-            forward.Normalize();
 
-            Vector3 left = TransformDirection(points[i], transform, PathSpace.xyz);
-            verts[vertIndex] = points[i] + left * texWidth * .5f;
-            verts[vertIndex + 1] = points[i] - left * texWidth * .5f;
-            Debug.Log(verts[vertIndex] + " " + verts[vertIndex + 1]);
+            forward.Normalize();
+            Vector3 left = new Vector3(forward.z, forward.y, -forward.x);
+
+            verts[vertIndex] = points[i] - left * texWidth * .5f;
+            verts[vertIndex + 1] = points[i] + left * texWidth * .5f;
+
+            float completionPercent = i / (float)(points.Length - 1);
+            float v = 1 - Mathf.Abs(2 * completionPercent - 1);
+            uvs[vertIndex] = new Vector2(1, v);
+            uvs[vertIndex + 1] = new Vector2(0, v);
 
             if (i < points.Length - 1)
             {
                 tris[triIndex] = vertIndex;
-                tris[triIndex + 1] = vertIndex + 2;
+                tris[triIndex + 1] = (vertIndex + 2) % verts.Length;
                 tris[triIndex + 2] = vertIndex + 1;
+
                 tris[triIndex + 3] = vertIndex + 1;
-                tris[triIndex + 4] = vertIndex + 2;
-                tris[triIndex + 5] = vertIndex + 3;
+                tris[triIndex + 4] = (vertIndex + 2) % verts.Length;
+                tris[triIndex + 5] = (vertIndex + 3) % verts.Length;
             }
 
             vertIndex += 2;
@@ -62,7 +73,7 @@ public class TexCreator : MonoBehaviour
         Mesh mesh = new Mesh();
         mesh.vertices = verts;
         mesh.triangles = tris;
-        mesh.RecalculateNormals();
+        mesh.uv = uvs;
         return mesh;
     }
 
