af4cd38a71da42047da54354f52e9d834162442b
WaniFi <chaos0824@naver.com>
2020-04-06 23:28:38 +0900

    강의 MeshCollider를 관리하기 위한 스크립트 추가

---
+++ Assets/Path Creator/RiverCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverCollider : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log(collision.gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+    }
+}
