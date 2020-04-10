ecc2334a6dea5a8a79e6118a1f751f17d02b41db
WaniFi <chaos0824@naver.com>
2020-04-10 22:47:24 +0900

    Scene 내 모든 Path Creator 들을 관리하는 스크립트 및 에디터 스크립트 구현

---
+++ Assets/Path Creator/Editor/RiverEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RiverManager))]
+public class RiverEditor : Editor
+{
+    private RiverManager manager;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+        if(GUILayout.Button("Create All River"))
+        {
+            manager.GenerateAllRivers();
+        }
+    }
+
+    private void OnEnable()
+    {
+        manager = (RiverManager)target;
+    }
+}
---
+++ Assets/Path Creator/RiverManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] RiverPaths;
+    [SerializeField]
+    private TerrainGenerator generator;
+    [SerializeField]
+    private int RestrictPos;
+    [SerializeField]
+    private float ReStrictRot;
+
+    // Start is called before the first frame update
+    public void GenerateAllRivers()
+    {
+        for (int i = 0; i < RiverPaths.Length; i++)
+        {
+            Vector3 pos = RiverPaths[i].transform.position;
+            Vector3 rot = RiverPaths[i].transform.rotation.eulerAngles;
+
+            if (pos.x == 0 || pos.x == 1024)
+                pos.z = Random.Range(RestrictPos, 1024 - RestrictPos);
+            else if (pos.z == 0 || pos.z == 1024)
+                pos.x = Random.Range(RestrictPos, 1024 - RestrictPos);
+            rot.y += (Random.Range(0, 1) == 0 ? 1 : -1) * ReStrictRot;
+            
+            RiverPaths[i].transform.position = pos;
+            RiverPaths[i].transform.rotation = Quaternion.Euler(rot);
+            RiverPaths[i].GetComponent<PathCreator>().CreateRandomRiver(1);
+        }
+        generator.GenerateRestrictHeights(10);
+    }
+}
