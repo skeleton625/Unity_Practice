c8e2232bba912e0cb18d06a494daa4e9456dc902
WaniFi <chaos0824@naver.com>
2020-04-21 23:42:36 +0900

    대화, 캐릭터 데이터 스크립트 추가

---
+++ Assets/AC-Dialogue/Scripts/Villager/DialogueData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue Data", menuName = "Dialogue")]
+public class DialogueData : ScriptableObject
+{
+    // 대화 정의 변수 ( Text 공간을 4 x 4 로 정의 )
+    [TextArea(4, 4)]
+    public List<string> conversationBlock;
+}
---
+++ Assets/AC-Dialogue/Scripts/Villager/VillagerData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+// 상단 메뉴 Asset에 VillagerData 스크립트 생성 버튼 추가
+[CreateAssetMenu(fileName = "New Villager", menuName = "Villager")]
+public class VillagerData : ScriptableObject
+{
+    public string vilalgerName;
+    public Color villagerColor;
+    public Color villagerNameColor;
+    public DialogueData dialogue;
+}
