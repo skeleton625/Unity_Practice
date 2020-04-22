e80eba3b886b100d29052cad15f83f346df38501
WaniFi <chaos0824@naver.com>
2020-04-22 23:44:05 +0900

    시민 대화 관리 스크립트 구현

--- Assets/AC-Dialogue/Scripts/Villager/VillagerScript.cs
+++ Assets/AC-Dialogue/Scripts/Villager/VillagerScript.cs
@@ -7,25 +7,25 @@ using Cinemachine;
 
 public class VillagerScript : MonoBehaviour
 {
-    [SerializeField]
-    private VillagerData villager;
-    [SerializeField]
-    private DialogueData diaogue;
+    public VillagerData villager;
+    public DialogueData dialogue;
     [SerializeField]
     private Renderer eyesRenderer;
     [SerializeField]
     private GameObject[] Particles;
     private Dictionary<string, ParticleSystem> EmotionParticles;
 
-    private bool villagerIsTalking;
-
+    private DialogueAudio dialogueAudio;
     private TMP_Animated animatedText;
     private Animator animator;
     private Camera mainCamera;
 
+    private bool villagerIsTalking;
+
     private void Start()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        dialogueAudio = GetComponent<DialogueAudio>();
 
         // 스크립트에 할당된 Particle 오브젝트를 딕셔너리에 추가
         for(int i = 0; i < Particles.Length; i++)
@@ -37,6 +37,7 @@ public class VillagerScript : MonoBehaviour
         animatedText.onEmotionChange.AddListener((newEmotion) => EmotionChanger(newEmotion));
     }
 
+    // Emotion 변수에 따라 캐릭터의 눈 표정을 변환시키는 함수
     private void EmotionChanger(Emotion e)
     {
         if (this != InterfaceManager.instance.currentVillager)
@@ -44,7 +45,7 @@ public class VillagerScript : MonoBehaviour
 
         animator.SetTrigger(e.ToString());
 
-        // Eye Material을 회전해 눈 감정 표현 정의
+        // Eye Material을 회전해 눈 표정을 정의
         if (e == Emotion.suprised)
             eyesRenderer.material.SetTextureOffset("_BaseMap", new Vector2(.33f, 0));
         else if (e == Emotion.angry)
@@ -62,21 +63,17 @@ public class VillagerScript : MonoBehaviour
         // action이 shake일 경우, 카메라를 흔들어주는 효과 작동
         if (action.Equals("shake"))
             mainCamera.GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+        // 다른 action 의 경우 -> 음성 action
         else
         {
+            // action에 해당하는 ParticleSystem 실행
             PlayParticle(action);
-
-            if (action.Equals("sparkle"))
-            {
-
-            }
-            else if (action.Equals("rain"))
-            {
-
-            }
+            // action에 해당하는 음성 출력
+            dialogueAudio.PlayAudioByAction(action);
         }
     }
 
+    // action 명으로 시작하는 ParticleSystem을 실행하는 함수
     private void PlayParticle(string action)
     {
         action += "Particle";
@@ -84,5 +81,30 @@ public class VillagerScript : MonoBehaviour
         if (EmotionParticles[action] == null)
             return;
         EmotionParticles[action].Play();
+    }
+
+    // 캐릭터 표정 초기화 함수
+    public void Reset()
+    {
+        animator.SetTrigger("normal");
+        eyesRenderer.material.SetTextureOffset("_BaseMap", Vector2.zero);
+    }
+
+    public void TurnToPlayer(Vector3 playerPos)
+    {
+        transform.DOLookAt(playerPos, (transform.position - playerPos).sqrMagnitude / 5);
+        string turnMotion = isRightSide(transform.forward, playerPos) ? "rturn" : "lturn";
+        animator.SetTrigger(turnMotion);
+    }
+
+    // 플레이어 캐릭터가 바라보고 있는 시민 캐릭터의 왼쪽, 오른쪽에 있는지 판별하는 함수
+    private bool isRightSide(Vector3 fwd, Vector3 targetDir)
+    {
+        // Vector3.Cross -> 두 매개변수 백터의 외적, 즉 fwd 방향의 오른쪽 백터
+        Vector3 right = Vector3.Cross(Vector3.up.normalized, fwd.normalized);
+        // Vector3.Dot -> 두 매개변수 백터간의 각도
+        float dir = Vector3.Dot(right, targetDir.normalized);
+        // -> 
+        return dir > 0f;
     }
 }
