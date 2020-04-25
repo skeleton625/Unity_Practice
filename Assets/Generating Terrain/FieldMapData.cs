0bddf4b974101d59ba0b23951428e91d4b8a6e06
WaniFi <chaos0824@naver.com>
2020-04-25 20:27:24 +0900

    TerrainInfo 스크립트를 ScriptableObject 상속 클래스로 전환 및 기존 함수들 이동

---
+++ Assets/Generating Terrain/FieldMapData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Field Map Data", menuName = "FieldData/Field Map")]
+public class FieldMapData : ScriptableObject
+{
+    [System.Serializable]
+    public struct SplatHeights
+    {
+        public int textureIndex;
+        public float startingHeight;
+    }
+    // Terrain의 가로, 세로, 깊이, 높낮이 규모 정의
+    /* Terrain의 가로, 세로 정의의 경우, 2의 제곱 수로 정의해야 함 */
+    public int Width, Height, Depth, Scale;
+    public float HeightLimit;
+    public Terrain FieldTerrain;
+    public SplatHeights[] TexBound;
+}
