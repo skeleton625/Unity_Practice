e2465045cf0e730024341a7403f67d033ae52928
WaniFi <chaos0824@naver.com>
2020-04-27 00:31:26 +0900

    플레이어의 입력을 통해 시민과 대화하는 스크립트 구현

--- Assets/AC-Dialogue/Scripts/DialogueTrigger.cs
+++ Assets/AC-Dialogue/Scripts/DialogueTrigger.cs
@@ -13,7 +13,7 @@ public class DialogueTrigger : MonoBehaviour
 
     [Space]
     [Header("Post Processing"), SerializeField]
-    private Volume dialougeDof;
+    public Volume dialogueDof;
 
     private void Start()
     {
@@ -23,16 +23,39 @@ public class DialogueTrigger : MonoBehaviour
 
     private void Update()
     {
-        
+        ActivateDialogueWithVillager();
     }
 
     private void ActivateDialogueWithVillager()
     {
+        // 현재 대화 대상이 존재하며, 대화 중이 아닐 시, 'F' 키를 통해 대화 시도
         if(Input.GetKeyDown(KeyCode.F) && !ui.inDialogue && currentVillager != null)
         {
-            // m_Targets[0] == Player Chracter
+            // m_Targets[0] ==> Player Chracter
             targetGroup.m_Targets[1].target = currentVillager.transform;
-            movement.enabled = false;
+            movement.active = false;
+            ui.InitiateDialogue();
+            currentVillager.TurnToPlayer(transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 시민 Object의 충돌체와 충돌한 경우, 해당 시민들 대화 대상으로 정의
+        if(other.CompareTag("Villager"))
+        {
+            currentVillager = other.GetComponent<VillagerScript>();
+            ui.currentVillager = currentVillager;
+        }
+    }
+
+    private void nTriggerExit(Collider other)
+    {
+        // 시민 Object의 충돌체에서 떨어진 경우, 대화 대상에서 제거
+        if(other.CompareTag("Villager"))
+        {
+            currentVillager = null;
+            ui.currentVillager = null;
         }
     }
 }
