9a1df82422201e7cf3de16dd59ac212f8a1aae7c
WaniFi <chaos0824@naver.com>
2020-04-26 22:34:43 +0900

    강 Mesh 병합 함수 이동 및 일부 상수값 세부 조정

--- Assets/Path Creator/PathCreator.cs
+++ Assets/Path Creator/PathCreator.cs
@@ -17,8 +17,7 @@ public class PathCreator : MonoBehaviour
     private TerrainGenerator generator;
     [SerializeField]
     public GameObject NewRiver;
-    [SerializeField]
-    public GameObject MainRiver;
+
 
     // Inspector 창에서 스크립트 컴포넌트를 Reset할 경우 실행
     private void Reset() { CreatePath(); }
@@ -29,14 +28,16 @@ public class PathCreator : MonoBehaviour
         path = new RiverPath(generator.SetRealHeight(transform.position));
     }
 
-    public void CreateRandomRiver(int BrushSize, float WaterLevel)
+    public void CreateRandomRiver(int DepthLevel, float WaterLevel)
     {
+        datas.WaterLevel = WaterLevel;
+        datas.DepthLevel = DepthLevel;
         int width = datas.RiverWidth;
         int height = datas.RiverHeight;
         int interval = datas.IntervalSize;
         CreatePath();
         // 랜덤 경로 지정
-        for(int x = 0; x <= width; x += interval)
+        for(int x = interval; x <= width; x += interval)
         {
             Vector3 riverPos = new Vector3(x, 0, Random.Range(-height, height));
             /* Vector.zero를 기준으로 transfrom.rotation 만큼 회전 */
@@ -49,35 +50,33 @@ public class PathCreator : MonoBehaviour
         }
 
         /* Terrain 깍기 시작 */
-        CreateRiver(BrushSize, WaterLevel);
+        CreateRiver(DepthLevel);
     }
 
     /* 현재 CreateRiver 함수는 CreateRandomRiver 함수에 종속적 (후에 병합 필요) */
-    public void CreateRiver(int BrushSize, float WaterLevel)
+    public void CreateRiver(int DepthLevel)
     {
         /* 강 생성을 위한 선 위의 점 좌표, 방문 여부 배열 정의 */
         Vector3[] points = path.CalculateEvenlySpacedPoitns(datas.spacing, datas.resolution);
         /* 강 생성 후, 해당 경로에 대한 새 경로 객체 정의 */
         RiverPath newPath = new RiverPath(points[0]);
 
+        int i = 1;
         /* 강 시작 점을 경로에 추가 */
-        Vector3 pos = generator.SetRealHeight(points[0]);
-        pos.y -= WaterLevel;
-        newPath.AddSegment(pos);
+        newPath.AddSegment(generator.SetRealHeight(points[0]));
         /* 시작, 끝 점을 제외한 나머지 점들에 대해 Terrain 높낮이를 조절 */
-        for (int i = 1; i < points.Length - 1; i++)
+        for (; i < points.Length - 1; i++)
         {
             int x = (int)points[i].x;
             int z = (int)points[i].z;
 
-            pos = generator.SetDownTerrain(x, z, BrushSize);
-            pos.y -= WaterLevel;
-            newPath.AddSegment(pos);
+            if (x >= 1024)
+                break;
+
+            newPath.AddSegment(generator.SetDownTerrain(x, z, DepthLevel));
         }
         /* 강 끝 점을 경로에 추가 */
-        pos = generator.SetRealHeight(points[points.Length - 1]);
-        pos.y -= WaterLevel;
-        newPath.AddSegment(pos);
+        newPath.AddSegment(generator.SetRealHeight(points[i]));
 
         /* 경로에 혼선을 주는 시작 점 좌표들 제거 */
         newPath.DeleteSegment(0);
@@ -86,7 +85,7 @@ public class PathCreator : MonoBehaviour
         path = newPath;
         path.AutoSetControlPoints = true;
 
-        UpdateTexture(newPath);
+        UpdateTexture(path);
     }
 
     /* Terrain의 상대적 높낮이(0~1)가 아닌 실제 높낮이를 계산하는 함수 */
@@ -98,7 +97,7 @@ public class PathCreator : MonoBehaviour
         return pos;
     }
 
-    public void UpdateTexture(RiverPath path)
+    private void UpdateTexture(RiverPath path)
     {
         GameObject River = Instantiate(NewRiver, Vector3.zero, Quaternion.identity);
         River.transform.position = new Vector3(0, -1f, 0);
@@ -108,7 +107,7 @@ public class PathCreator : MonoBehaviour
         River.GetComponent<MeshCollider>().sharedMesh = RiverMesh;
 
         gameObject.SetActive(false);
-        River.transform.parent = MainRiver.transform;
+        River.transform.parent = GameObject.Find("MainRiver").transform;
     }
 
     private Mesh CreateTexMesh(RiverPath path)
@@ -117,6 +116,7 @@ public class PathCreator : MonoBehaviour
         Vector2[] uvs = new Vector2[verts.Length];
         int[] tris = new int[2 * (path.NumPoints - 1) * 3];
         int vertIndex = 0, triIndex = 0;
+        int[] riverWidth = new int[3] { 5, 15, 30 };
 
         for (int i = 0; i < path.NumPoints; i += 3)
         {
@@ -125,12 +125,12 @@ public class PathCreator : MonoBehaviour
                 forward += path[(i + 1) % path.NumPoints] - path[i];
             if (i > 0)
                 forward += path[i] - path[(i - 1 + path.NumPoints) % path.NumPoints];
-
             forward.Normalize();
             Vector3 left = new Vector3(forward.z, forward.y, -forward.x);
-
-            verts[vertIndex] = path[i] - left * 10;
-            verts[vertIndex + 1] = path[i] + left * 10;
+            verts[vertIndex] = generator.SetRealHeight(path[i] - left * riverWidth[datas.DepthLevel]);
+            verts[vertIndex + 1] = generator.SetRealHeight(path[i] + left * riverWidth[datas.DepthLevel]);
+            verts[vertIndex].y -= (datas.WaterLevel + 0.1f);
+            verts[vertIndex + 1].y -= (datas.WaterLevel + 0.1f);
 
             float completionPercent = i / (float)(path.NumPoints - 1);
             float v = 1 - Mathf.Abs(2 * completionPercent - 1);
@@ -159,23 +159,5 @@ public class PathCreator : MonoBehaviour
         mesh.RecalculateNormals();
         mesh.uv = uvs;
         return mesh;
-    }
-
-    public void SetCombineAllRiverMesh()
-    {
-        MeshFilter[] meshes = MainRiver.transform.GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] combine = new CombineInstance[meshes.Length];
-
-        for (int i = 0; i < meshes.Length; i++)
-        {
-            combine[i].mesh = meshes[i].sharedMesh;
-            combine[i].transform = meshes[i].transform.localToWorldMatrix;
-            meshes[i].gameObject.SetActive(false);
-        }
-
-        MeshFilter RiverMesh = MainRiver.GetComponent<MeshFilter>();
-        RiverMesh.mesh = new Mesh();
-        RiverMesh.mesh.CombineMeshes(combine);
-        MainRiver.SetActive(true);
     }
 }
